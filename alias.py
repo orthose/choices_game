5c0fe953f264afae1ebf7f4772c030bb49b24bb8
maxime vincent <maxime.vincent1@u-psud.fr>
2020-08-09 12:11:56 +0200

    Ajout de alias.py

---
+++ alias.py
@@ -0,0 +1,13 @@
+#####################################################
+#                                                   #
+# Jeu à choix multiples édité sous licence GNU v3.0 #
+# https://github.com/orthose/choices_game.git       #
+# Auteur: Maxime Vincent                            #
+#                                                   #
+#####################################################
+
+# Dictionnaire des alias permettant de personnaliser
+# certaines variables importantes du texte
+alias = {
+    "@game": "La Guerre de Proxima"
+}
